cd4ee853a7e2bf7374a2be54cd6ef6e8664b1ab8
shadow3aaa <shadow3aaa@qq.com>
2024-04-19 08:35:51 +0000

    在ebpf向用户态发送渲染信号

---
+++ frame-analyzer-ebpf/src/main.rs
@@ -0,0 +1,35 @@
+#![no_std]
+#![no_main]
+
+use aya_ebpf::{
+    macros::{uprobe, map},
+    programs::ProbeContext,
+    maps::RingBuf,
+};
+
+use frame_analyzer_ebpf_common::FrameSignal;
+
+#[map]
+static RING_BUF: RingBuf = RingBuf::with_byte_size(0, 0);
+
+#[uprobe]
+pub fn frame_analyzer_ebpf(ctx: ProbeContext) -> u32 {
+    match try_frame_analyzer_ebpf(ctx) {
+        Ok(ret) => ret,
+        Err(ret) => ret,
+    }
+}
+
+fn try_frame_analyzer_ebpf(_ctx: ProbeContext) -> Result<u32, u32> {
+    if let Some(mut entry) = RING_BUF.reserve::<FrameSignal>(0) {
+        entry.write(FrameSignal);
+        entry.submit(0);
+    }
+    
+    Ok(0)
+}
+
+#[panic_handler]
+fn panic(_info: &core::panic::PanicInfo) -> ! {
+    unsafe { core::hint::unreachable_unchecked() }
+}
