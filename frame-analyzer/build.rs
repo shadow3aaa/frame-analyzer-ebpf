77cd3443379674431407491f4a5988581a61796e
shadow3aaa <shadow3aaa@qq.com>
2024-04-21 12:04:03 +0800

    自动下载bpf-linker

--- frame-analyzer/build.rs
+++ frame-analyzer/build.rs
@@ -18,10 +18,37 @@
  */
 use std::{env, fs, path::Path, process::Command};
 
-use anyhow::Result;
+use anyhow::{Ok, Result};
 
 fn main() -> Result<()> {
+    install_ebpf_linker()?;
     build_ebpf()?;
+    Ok(())
+}
+
+fn add_path<S: AsRef<str>>(add: S) -> Result<String> {
+    let path = env::var("PATH")?;
+    Ok(format!("{path}:{}", add.as_ref()))
+}
+
+fn install_ebpf_linker() -> Result<()> {
+    let out_dir = env::var("OUT_DIR")?;
+    let out_dir = Path::new(&out_dir);
+    let target_dir = out_dir.join("temp_target");
+    let target_dir_str = target_dir.to_str().unwrap();
+
+    Command::new("cargo")
+        .args([
+            "install",
+            "bpf-linker",
+            "--force",
+            "--root",
+            target_dir_str,
+            "--target-dir",
+            target_dir_str,
+        ])
+        .status()?;
+
     Ok(())
 }
 
@@ -29,8 +56,11 @@ fn build_ebpf() -> Result<()> {
     let current_dir = env::current_dir()?;
     let project_path = current_dir.parent().unwrap().join("frame-analyzer-ebpf");
     let out_dir = env::var("OUT_DIR")?;
-    let target_dir = Path::new(&out_dir).join("ebpf_target");
+    let out_dir = Path::new(&out_dir);
+    let target_dir = out_dir.join("ebpf_target");
     let target_dir_str = target_dir.to_str().unwrap();
+    let bin = out_dir.join("temp_target").join("bin");
+    let bin = bin.to_str().unwrap();
 
     if !target_dir.exists() {
         fs::create_dir(&target_dir)?;
@@ -47,17 +77,18 @@ fn build_ebpf() -> Result<()> {
 
     if project_path.exists() {
         println!("cargo:rerun-if-changed=../frame-analyzer-ebpf");
+
         #[cfg(not(debug_assertions))]
         ebpf_args.push("--release");
-        // println!("cargo::warning=Building ebpf from workspace");
+
         Command::new("cargo")
             .arg("build")
             .args(ebpf_args)
             .env_remove("RUSTUP_TOOLCHAIN")
             .current_dir(&project_path)
+            .env("PATH", add_path(bin)?)
             .status()?;
     } else {
-        // println!("cargo::warning=Building ebpf from crates.io");
         #[cfg(debug_assertions)]
         ebpf_args.push("--debug");
 
@@ -66,6 +97,7 @@ fn build_ebpf() -> Result<()> {
             .args(["install", "frame-analyzer-ebpf"])
             .args(ebpf_args)
             .args(["--root", target_dir_str])
+            .env("PATH", add_path(bin)?)
             .status()?;
 
         #[cfg(debug_assertions)]
